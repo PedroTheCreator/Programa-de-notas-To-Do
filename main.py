96e59874b96715cd129b129d699c25f98695a738
Pedro Henrique R. da Mata <pedrohenrique.phr38@gmail.com>
2020-10-17 23:00:55 +0000

    v2.0: Funcionamento com múltiplos arquivos

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+from colorama import Fore, init
+init(autoreset=True)
 def apresentar():
-  print("===== To do List =====\n1 --> Inserir uma nova nota\n2 --> Ver todas as notas\n3 --> Excluir uma nota\n4 --> Limpar\n5 --> Sair")
-  x = int(input("Insira sua escolha: "))
+  print(Fore.LIGHTYELLOW_EX + "===== To do List =====\n" + Fore.RESET + "1 --> Inserir uma nova nota\n2 --> Ver todas as notas\n3 --> Excluir uma nota\n4 --> Limpar\n5 --> Copiar lista\n6 --> Alterar arquivo de leitura\n7 --> Sair")
+  x = int(input(Fore.LIGHTYELLOW_EX +"Insira sua escolha: "))
   os.system('clear')
   return x
 
@@ -22,15 +24,15 @@ def limpar_tela():
 #Criação do arquivo, caso não exista.
 criar("notas.txt")
 escolha = 0 # Declaração para iniciar o loop.
-
-while(escolha != 5):
+arquivo = "notas.txt"
+while(escolha != 7):
   #Varredura do arquivo
-  arq = open("notas.txt","r") # Abro o arquivo em modo leitura
+  arq = open(arquivo,"r") # Abro o arquivo em modo leitura
   primeira_linha = arq.readline() # Leio a primeira linha onde contém o total
   try:
     total = int(primeira_linha[16:(len(primeira_linha))]) #Busca o número total de notas, caso não exista um total ele cai no except.
   except:
-    limpar_nota("notas.txt", True) # Cria o contador iniciando em 0
+    limpar_nota(arquivo, True) # Cria o contador iniciando em 0
     total = int(primeira_linha[16:(len(primeira_linha))]) # Passa isso para a varíavel total.
   conteudo = arq.readlines() # Recebe tudo que está abaixo do contador.
   arq.close()
@@ -38,53 +40,71 @@ while(escolha != 5):
   #Funções do menu
   if escolha == 1:
     nota = str(input("Insira sua nota: ")) # Recebo a nota
-    limpar_nota("notas.txt", False) # Limpo a nota, sem deixar o texto padrão.
-    arq = open("notas.txt", "a+") # Abro no modo adição no fim.
+    limpar_nota(arquivo, False) # Limpo a nota, sem deixar o texto padrão.
+    arq = open(arquivo, "a+") # Abro no modo adição no fim.
     total += 1 # Acréscimo no total recebido na rotina de loop.
     arq.write("Total de notas: " + str(total) +"\n") # Inserção do texto padrão com o total atualizado
     if (total-1) > 0: #Prevenção desnecessária.
       arq.writelines(conteudo) # Inserção do conteudo já existente.
     arq.write(str(total) + ". " + nota + "\n") #Inserção da nova nota, mantendo o padrão de entrada.
     arq.close()
-    print("\nNOTA INSERIDA COM SUCESSO") # Feedback ao usuário
+    print(Fore.GREEN + "\nNOTA INSERIDA COM SUCESSO") # Feedback ao usuário
     limpar_tela() #Função extra, consulte no slide.
   elif escolha == 2:
-    print("Você tem {} notas\nSuas notas: \n".format(total))
+    print(Fore.LIGHTYELLOW_EX + "Você tem {} notas\nSuas notas: \n".format(total))
     for i in conteudo:
       print(i)
-    input("Pressione ENTER para continuar.")
+    input(Fore.LIGHTYELLOW_EX + "Pressione ENTER para continuar.")
     os.system("clear")
   elif escolha == 3:
     print("Você tem {} notas\nSuas notas: \n".format(total)) #Repete a visualização
     for i in conteudo:
       print(i)
-    apagador = int(input("Insira a nota que deseja apagar: ")) # Recebo o índice
+    apagador = int(input(Fore.LIGHTYELLOW_EX + "Insira a nota que deseja apagar: ")) # Recebo o índice
     conteudo.pop((apagador-1)) # Apago da lista, com descréscimo, pois a lista começa em 0.
     total-=1 #Decréscimo no total
-    limpar_nota("notas.txt",False) # Limpo a nota sem a entrada padrão. Consulte "Funções para organização" no slide.
-    arq = open("notas.txt", "a+") # Abro o arquivo em modo adição no final.
+    limpar_nota(arquivo,False) # Limpo a nota sem a entrada padrão. Consulte "Funções para organização" no slide.
+    arq = open(arquivo, "a+") # Abro o arquivo em modo adição no final.
     arq.write("Total de notas: " + str(total) +"\n") #Insiro o novo contador
     contador = 0 #Defino um contador para organizar os elementos
     for i in conteudo: # Loop
       contador+=1 #Acréscimo do contador no começo, pois nossas notas começam em 1.
       arq.write(str(contador) + ". " + (i[3:(len(i))])) #Insiro o contador, o padrão de entrada e,após os 3 primeiros caracteres que são correspondente ao indíce, insiro o conteúdo da nota. Ex: "1. " = 3 caracteres. Por isso o i[3:], ou seja, somente o que está após do índice. 
     arq.close() #Sempre fecha o arquivo que abriu, caso contrário, não será atualizado em tempo real e irá gerar bugs na execução.
-    print("\nNOTA EXCLUIDA COM SUCESSO") # Feedback para o usuário.
+    print(Fore.RED + "\nNOTA EXCLUIDA COM SUCESSO") # Feedback para o usuário.
     limpar_tela() # Função de organização.
   elif escolha == 4:
     confirmacao = str(input('Insira "Sim" caso deseje apagar todas as suas notas.\nDigite: ')) #Input de confirmação.
     if confirmacao.lower() == "sim": # Comparação.
-      limpar_nota("notas.txt", True) # Função de organização.
+      limpar_nota(arquivo, True) # Função de organização.
       conteudo.clear() # Limpeza na lista recebida da rotina de loop.
       total = 0 # Zerar o contador.
-      print("Notas limpas com sucesso!") # Feedback ao usuário.
+      print(Fore.RED + "Notas limpas com sucesso!") # Feedback ao usuário.
       limpar_tela() # Função de organização.
     else:
-      print("Suas notas foram mantidas.")
+      print(Fore.LIGHTGREEN_EX + "Suas notas foram mantidas.")
       limpar_tela() # Função de organização.
   elif escolha == 5:
-    print("Programa encerrado!")
-    pass
+    copia = str(input("Deseja salvar com qual nome: "))
+    copia+=".txt"
+    criar(copia)
+    carq = open(copia, "a+")
+    carq.write(primeira_linha)
+    carq.writelines(conteudo)
+    carq.close()
+    limpar_tela()
+  elif escolha == 6:
+    print("Arquivo lido atualmente: {}".format(arquivo))
+    if str(input("Deseja alterar? Caso queira, digite 'Sim'.\nInsira: ")).lower() == "sim":
+      arq.close()
+      arquivo = ""
+      arquivo = str(input("Insira o arquivo que deseja ler: "))
+      arquivo+=".txt"
+      conteudo.clear()
+      primeira_linha = ""
+  elif escolha == 7:
+    print(Fore.RED + "Programa encerrado!")
+    #pass
   else:
-    print("Escolha inválida, tente novamente.")
+    print(Fore.LIGHTRED_EX + "Escolha inválida, tente novamente.")
     limpar_tela()
